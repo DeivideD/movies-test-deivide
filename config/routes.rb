b68abde146f2b43716665c5c6d3ef71af3ceb47a
Deivide Duarte <deivideduarte@fortestecnologia.com.br>
2023-02-05 20:39:36 -0300

    update swagger rout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => '/api/v1/doc'
+  mount Rswag::Api::Engine => '/api/v1/doc'
   mount Movies::API => '/'
   mount Ratings::API => '/'
 end
